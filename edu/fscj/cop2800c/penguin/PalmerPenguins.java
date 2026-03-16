79edf31a2dbd60d46b44108de0e5c345d3dcc92f
jelena677 <S3721135@students.fscj.edu>
2026-03-15 20:14:32 -0400

    Update PalmerPenguins.java

--- edu/fscj/cop2800c/penguin/PalmerPenguins.java
+++ edu/fscj/cop2800c/penguin/PalmerPenguins.java
@@ -1,14 +1,20 @@
 // PalmerPenguins.java
-// D. Singletary
-// 2/22/25
+// Jelena Radovanovic
+// 3/15/2026
 // Refactored Palmer Penguins analysis using object-oriented approach
 
 package edu.fscj.cop2800c.penguin;
 
 public class PalmerPenguins {
     public static void main(String[] args) {
-        // Create an instance of PenguinAnalyzer
-
+// Create an instance of PenguinAnalyzer
+        PenguinAnalyzer analyzer = new PenguinAnalyzer();
+// Read CSV data
+        analyzer.readPenguinData();
+// Print formatted output
+        analyzer.printPenguinData();
+// Save results to a file
+        analyzer.writePenguinData();
 
     }
 }
